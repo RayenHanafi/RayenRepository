142ac2d33170b64bfa299fc0163db24d15390dd2
RayenHanafi <rayen.hanafi@insat.ucar.tn>
2025-02-17 22:49:33 +0100

    todo missing delete button

--- TODO/index3.js
+++ TODO/index3.js
@@ -5,6 +5,10 @@ let todoList = document.getElementById('todoList');
 
 
 document.getElementById('addTodo').addEventListener('click', () => {
+    if (name.value.trim() === "" || content.value.trim() === "") {
+      alert("Please fill in both fields.");
+      return;
+    }
     const todo = document.createElement('li');
     todo.textContent = name.value + ' : ' + content.value;
     todo.classList.add('list-group-item');
